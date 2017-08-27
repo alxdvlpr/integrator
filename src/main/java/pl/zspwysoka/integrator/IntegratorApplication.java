3a13a19351e03a28a9d5ec48ea094983d71b1416
Alex Dev <alxdvlpr@gmail.com>
2017-08-27 22:17:44 +0200

    init

---
+++ src/main/java/pl/zspwysoka/integrator/IntegratorApplication.java
@@ -0,0 +1,12 @@
+package pl.zspwysoka.integrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegratorApplication.class, args);
+	}
+}
